0bc67ef6d44557be6ab6a4e5897ded14203e1ae4
TonySavich <92541859+TonySavich@users.noreply.github.com>
2022-09-12 22:13:44 +0200

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector <char> s;
+	
+	std::string d;
+	std::cin >> d;
+	for (int i = 0; i < d.size()+1; i++) {
+		s.push_back(d[i]);
+	}
+
+
+	for (int i = 0; i < s.size(); i++) {
+	
+		if (s[i] == ')' || s[i] == ']' || s[i] == '}') {
+		
+			auto iter = s.cbegin();
+			if ((i - 1) > (-1)) {
+				if ((s[i - 1] == '(' && s[i] == ')' )||( s[i - 1] == '[' && s[i] == ']') || (s[i - 1] == '{' && s[i] == '}')) {
+
+					s.erase(iter + i);
+					s.erase(iter + i - 1);
+					i = 0;
+				}
+			}
+		}
+   }
+	
+	
+	if (s.size() < 2) {
+		std::cout << "true";
+
+	}
+	else {
+		std::cout << "false";
+	}
+
+//	for (int i = 0; i < s.size(); i++) {
+	//	std::cout << s[i];
+	//}
+
+}
